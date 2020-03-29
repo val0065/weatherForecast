e484f84417b64ca50ee910d69a3c52149004acb6
david <woozie28@gmail.com>
2020-03-29 20:18:32 +0200

    responsiveness

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 function assignCity() {
-    let city;
+    let city = 'Ostrava';
     let tempDescriptionDetail = document.querySelector('.temp-description-detail');
     let tempDegree = document.querySelector('.temp-degree');
     let locationTimezone = document.querySelector('.location-timezone');
     let tempIcon = document.querySelector('.temp-icon');
     city = document.getElementById("city").value;
+    console.log(city);
 
     const proxy = 'https://cors-anywhere.herokuapp.com/';
     const api = `${proxy}api.openweathermap.org/data/2.5/weather?q=${city}&lang=cz&appid=35eadc12a742fadf5f97b1c0db70121f`;
@@ -19,10 +20,8 @@ function assignCity() {
             tempDegree.textContent = celsius;
             let desc = data.weather[0].description;
             tempDescriptionDetail.textContent = desc.toUpperCase();
-            locationTimezone.textContent = data.name;
+            locationTimezone.textContent = data.name + ', ' + data.sys.country;
             let icon = data.weather[0].icon;
             tempIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-            let dawn = data.sys.sunrise;
-            let dusk = data.sys.sunset;
         });
 }
